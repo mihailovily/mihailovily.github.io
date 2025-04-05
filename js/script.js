70c8fa548afd8eb78821ca25ff63171b7e518736
Ilya <mihailovily@gmail.com>
2025-04-05 22:31:19 +0300

    code refactoring

--- js/script.js
+++ js/script.js
@@ -1,23 +1,31 @@
-/*$(document).ready(function () {
-    $(".menu").hide();
-    $(".hamburger").click(function () {
-        $(".menu").slideToggle("slow", function () {
-            $("#burger").addClass("cross");
-            $("#burger").removeClass("hamburger");
-        });
-    });
+// Simple terminal typing effect
+document.addEventListener('DOMContentLoaded', function() {
+    const terminal = document.querySelector('.terminal-content');
+    const commands = [
+        "nmap -sV -T4 target.com"
+    ];
 
-    $(".cross").click(function () {
-        $(".menu").slideToggle("slow", function () {
-            $("#burger").addClass("hamburger");
-            $("#burger").removeClass("cross");
-        });
-    });
+    let currentCommand = 0;
 
-});
-*/
-$(document).ready(function () {
-    $("#burger").click(function () {
-        $(".menu").toggle(1000);
-    });
+    function typeNextCommand() {
+        if (currentCommand < commands.length) {
+            const prompt = terminal.querySelector('span:last-child');
+            prompt.classList.remove('blink');
+
+            const newLine = document.createElement('div');
+            newLine.innerHTML = `$ ${commands[currentCommand]}<br><span>> Command executed successfully</span><br><br><span>$ <span class="blink">_</span></span>`;
+            terminal.appendChild(newLine);
+
+            currentCommand++;
+            prompt.classList.add('blink');
+
+            // Auto-scroll to bottom
+            terminal.scrollTop = terminal.scrollHeight;
+
+            // Schedule next command
+            setTimeout(typeNextCommand, 3000);
+        }
+    }
+
+    setTimeout(typeNextCommand, 5000);
 });
